ad88dd69a44776efa1e7304d1c94a981c8c4256f
Praneet Karna <praneetkarna77@gmail.com>
2026-06-17 15:45:55 +0530

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.py
@@ -0,0 +1,49 @@
+class Solution:
+
+    def impl2(self, nums: List[int]) -> int:
+        s = set()
+        for num in nums: 
+            if(num not in s): s.add(num)
+        
+        max = 0
+
+        for num in s:
+            if(num-1 not in s):
+                len = 1
+                num += 1
+                while(num in s):
+                    num += 1
+                    len += 1
+                
+                if(len>max): max = len
+        
+        return max
+                
+
+    def get(self, num: int, s: Set[int], mem: {}) -> int:
+        if num in s: 
+            mem[num] = 1 + self.get(num+1, s, mem) 
+            return mem[num]
+        else: 
+            return 0
+
+    def impl1(self, nums: List[int]) -> int:
+        s = set()
+        for num in nums: 
+            if(num not in s): s.add(num)
+        
+        mem = {}
+
+        max = 0
+        for num in s: 
+            if(num not in mem): self.get(num, s, mem)
+        
+        for value in mem.values():
+            if(value>max): max = value
+        return max
+                
+
+
+
+    def longestConsecutive(self, nums: List[int]) -> int:
+        return self.impl2(nums)
